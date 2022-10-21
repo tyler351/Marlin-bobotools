1280c1bae92717a14168d3b8ef50c1a22bb68ebb
Tyler Keeton <tylerkeeton17@gmail.com>
2022-10-21 16:10:46 -0400

    Update Configuration.h
    adusted preheat settings again. Now we only preheat when bedleveling is happening.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1499,7 +1499,7 @@
 //#define DELAY_BEFORE_PROBING 200  // (ms) To prevent vibrations from triggering piezo sensors
 
 // Require minimum nozzle and/or bed temperature for probing
-#define PREHEAT_BEFORE_PROBING
+// #define PREHEAT_BEFORE_PROBING
 #if ENABLED(PREHEAT_BEFORE_PROBING)
   #define PROBING_NOZZLE_TEMP 120   // (°C) Only applies to E0 at this time
   #define PROBING_BED_TEMP     50
@@ -1765,7 +1765,7 @@
 /**
  * Auto-leveling needs preheating
  */
-// #define PREHEAT_BEFORE_LEVELING
+#define PREHEAT_BEFORE_LEVELING
 #if ENABLED(PREHEAT_BEFORE_LEVELING)
   #define LEVELING_NOZZLE_TEMP 120   // (°C) Only applies to E0 at this time
   #define LEVELING_BED_TEMP     50
